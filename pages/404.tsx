4f94322229a5cd1ec5567f3482bad6115b25b474
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2022-04-29 15:12:01 +0700

    fix timer 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,22 +1,26 @@
 import router from "next/router";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Head from "next/head";
 import s from "../styles/Home.module.css";
 
 const Custom_404 = () => {
   const [content, setContent] = useState("");
-
+  const i = useRef(0);
   useEffect(() => {
     setTimeout(() => {
       if (content == "") {
         setContent("Redirecting to Home.");
       } else {
         setContent(content + ".");
+        i.current++;
+      }
+      console.log(i.current);
+      if (i.current === 3) {
+        router.push("/");
       }
     }, 1000);
-    setTimeout(() => {
-      router.push("/");
-    }, 4000);
+    // setTimeout(() => {
+    // }, 4000);
   }, [content]);
 
   return (
